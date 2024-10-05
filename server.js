73ad9a306f3065e67ca8490b69dd0580ca2c263c
Void <secretceeper25@gmail.com>
2024-10-05 23:03:09 +0300

    sql подключен

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-const bootstrap = require('bootstrap')
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "testchat",
+    password: "Darkness"
+});
+
+// тестирование подключения
+connection.connect(function(err){
+    if (err) {
+        return console.error("Ошибка: " + err.message);
+    }
+    else{
+        console.log("Подключение к серверу MySQL успешно установлено");
+    }
+});
+
+connection.query(
+    'SELECT * FROM `acces`',
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+);
+
+ // закрытие подключения
+connection.end(function(err) {
+if (err) {
+    return console.log("Ошибка: " + err.message);
+}
+console.log("Подключение закрыто");
+});

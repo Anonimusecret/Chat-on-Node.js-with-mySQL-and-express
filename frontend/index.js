f71d9650d3a0d79f47e9afe901cf5891346cc534
Void <secretceeper25@gmail.com>
2024-10-05 15:54:26 +0300

    Окошко логина готово

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+ function redirect(){
+    let url = 'https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage'
+    if(true){
+        location.href="./login.html"
+     }
+ }
